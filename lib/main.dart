31e9d9edba889889c7213c6671a12aa4afc79945
Julio Cons <jconscalderon@hotmail.com>
2022-01-03 21:41:56 -0700

    ⚡️ First Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CallApp());
+
+class CallApp extends StatelessWidget {
+  const CallApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        body: Container(
+          child: const Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
