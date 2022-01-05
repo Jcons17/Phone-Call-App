939449fb1a5a2f1e5c7cd8d3c0a9028870673d99
Julio Cons <jconscalderon@hotmail.com>
2022-01-04 21:21:28 -0700

    👔  Add Contact Model

---
+++ lib/model/contact_model.dart
@@ -0,0 +1,12 @@
+class Contact{
+
+  final int idContact;
+  final String firstName;
+  final String? lastName;
+  final String numberPhone;
+  final String? pathImage;
+  final String? email;
+
+  Contact({required this.idContact, required this.firstName, this.lastName, required this.numberPhone, this.email, this.pathImage});
+
+} 
