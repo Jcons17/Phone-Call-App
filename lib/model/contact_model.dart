dabf06901d2156e749ca25c56760985949fe3b6e
Julio Cons <jconscalderon@hotmail.com>
2022-01-18 20:01:50 -0700

    🎨  Add data generate to model contacts

--- lib/model/contact_model.dart
+++ lib/model/contact_model.dart
@@ -1,12 +1,93 @@
-class Contact{
+import 'dart:convert';
+
+class Contact {
 
   final int idContact;
   final String firstName;
   final String? lastName;
   final String numberPhone;
   final String? pathImage;
-  final String? email;
+  final String? email;  
 
-  Contact({required this.idContact, required this.firstName, this.lastName, required this.numberPhone, this.email, this.pathImage});
+  Contact({
+    required this.idContact,
+    required this.firstName,
+    this.lastName,
+    required this.numberPhone,
+    this.pathImage,
+    this.email,
+  });
 
+
+  Contact copyWith({
+    int? idContact,
+    String? firstName,
+    String? lastName,
+    String? numberPhone,
+    String? pathImage,
+    String? email,
+  }) {
+    return Contact(
+      idContact: idContact ?? this.idContact,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      numberPhone: numberPhone ?? this.numberPhone,
+      pathImage: pathImage ?? this.pathImage,
+      email: email ?? this.email,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'idContact': idContact,
+      'firstName': firstName,
+      'lastName': lastName,
+      'numberPhone': numberPhone,
+      'pathImage': pathImage,
+      'email': email,
+    };
+  }
+
+  factory Contact.fromMap(Map<String, dynamic> map) {
+    return Contact(
+      idContact: map['idContact']?.toInt() ?? 0,
+      firstName: map['firstName'] ?? '',
+      lastName: map['lastName'],
+      numberPhone: map['numberPhone'] ?? '',
+      pathImage: map['pathImage'],
+      email: map['email'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Contact.fromJson(String source) => Contact.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Contact(firstName: $firstName, numberPhone: $numberPhone)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is Contact &&
+      other.idContact == idContact &&
+      other.firstName == firstName &&
+      other.lastName == lastName &&
+      other.numberPhone == numberPhone &&
+      other.pathImage == pathImage &&
+      other.email == email;
+  }
+
+  @override
+  int get hashCode {
+    return idContact.hashCode ^
+      firstName.hashCode ^
+      lastName.hashCode ^
+      numberPhone.hashCode ^
+      pathImage.hashCode ^
+      email.hashCode;
+  }
 } 
